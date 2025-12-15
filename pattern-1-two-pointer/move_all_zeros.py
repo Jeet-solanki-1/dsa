b6732476d296c096162caa57aa4d28aa94289f62
Jeet-solanki-1 <solankijeet285@gmail.com>
2025-12-15 14:16:47 +0530

    Refactor comments for clarity and add main execution block in move_all_zeros.py and remove_duplicate.py

--- pattern-1-two-pointer/move_all_zeros.py
+++ pattern-1-two-pointer/move_all_zeros.py
@@ -1,6 +1,7 @@
 # 283. Move Zeroes
 
-# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Given an integer array nums, move all 0's to
+#  the end of it while maintaining the relative order of the non-zero elements.
 
 # Note that you must do this in-place without making a copy of the array.
 
@@ -14,4 +15,22 @@
 
 # Input: nums = [0]
 # Output: [0]
- 
+class Sol:
+    def remove_zero(self,nums:list[int]) -> None:
+        # In-place
+        # call by reference
+        slow=0
+        for fast in range(len(nums)):
+            if nums[fast]!=0:
+                nums[slow]=nums[fast]
+                slow+=1
+
+        for i in range(slow,len(nums)):
+            nums[i]=0
+
+if __name__ == "__main__":
+    s = Sol()
+    nums= [0,2,0,3,4,4,3,0,0]
+    s.remove_zero(nums=nums)
+    print(nums)
+    
--- pattern-1-two-pointer/remove_duplicate.py
+++ pattern-1-two-pointer/remove_duplicate.py
@@ -1,10 +1,13 @@
 # 26. Remove Duplicates from Sorted Array
 
-# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element 
+# appears only once. The relative order of the elements should be kept the same.
 
-# Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+# Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates,
+#  return the number of unique elements k.
 
-# The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+# The first k elements of nums should contain the unique numbers in sorted order. 
+# The remaining elements beyond index k - 1 can be ignored.
 
 # Custom Judge:
 
@@ -35,3 +38,23 @@
 # Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 # Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
+
+class Solution:
+    def remove_duplicate(self,nums:list[int]) -> int:
+        slow=0
+        for fast in range(len(nums)):
+            if slow==0 or nums[fast]!=nums[fast-1]:
+                nums[slow]=nums[fast]
+                slow+=1
+        print(nums)
+        return slow
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    output = 5
+    if output==s.remove_duplicate(nums=nums):
+        print("yes")
+    else:
+        print("no")
+    
